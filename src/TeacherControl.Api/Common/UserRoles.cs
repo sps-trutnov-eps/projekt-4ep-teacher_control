470ded74bf7b436f9a66a1da5eb63f7cde4f49e5
Matěj Kubíček <kubicekm23@spstrutnov.cz>
2026-09-13 20:39:25 +0200

    Přidání atributů uživatelům
    Přidání rolí, celého jména a bool zda jde o učitele.

---
+++ src/TeacherControl.Api/Common/UserRoles.cs
@@ -0,0 +1,8 @@
+namespace TeacherControl.Api.Common;
+
+public enum UserRoles
+{
+    User = 0,
+    Moderator = 1,
+    Administrator = 2
+}
--- src/TeacherControl.Api/Entities/UserEntity.cs
+++ src/TeacherControl.Api/Entities/UserEntity.cs
@@ -1,7 +1,18 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
+using TeacherControl.Api.Common;
 
 namespace TeacherControl.Api.Entities;
 
 public class UserEntity : IdentityUser
 {
+    [MaxLength(50)]
+    public string? FullName { get; set; } // username = windows login, FullName = actual jméno
+
+    /// <summary>
+    /// Lze zjistit tak, že UserName obsahuje čísla.
+    /// </summary>
+    public bool IsTeacher { get; set; } = false;
+
+    public UserRoles Role { get; set; } = UserRoles.User;
 }
