82492023362d8fc2983864c9fa6bb6adea9cdbdb
Matěj Kubíček <kubicekm23@spstrutnov.cz>
2026-09-13 20:51:02 +0200

    masters/backend: přidání prázdných entit linknutých na učitele
    Opice, je potřeba zvlášť tabulka učitelů, protože jinak by se musel každý učitel někdy přihlásit, aby ho někdo mohl hodnotit a podobně.

--- src/TeacherControl.Api/Entities/UserEntity.cs
+++ src/TeacherControl.Api/Entities/UserEntity.cs
@@ -9,10 +9,7 @@ public class UserEntity : IdentityUser
     [MaxLength(50)]
     public string? FullName { get; set; } // username = windows login, FullName = actual jméno
 
-    /// <summary>
-    /// Lze zjistit tak, že UserName obsahuje čísla.
-    /// </summary>
-    public bool IsTeacher { get; set; } = false;
+    public int? Teacher { get; set; } = null;
 
     public UserRoles Role { get; set; } = UserRoles.User;
 }
